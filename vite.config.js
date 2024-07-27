c2a07bbcabe5c1f64bafdb2decec4ac2e2a3d53c
wstypr <wsetypeermadi@gmail.com>
2024-07-28 03:56:42 +0700

    fix index page

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "play.html"),
+        nested: resolve(__dirname, "compose.html"),
+        nested2: resolve(__dirname, "play.html"),
       },
     },
   },
